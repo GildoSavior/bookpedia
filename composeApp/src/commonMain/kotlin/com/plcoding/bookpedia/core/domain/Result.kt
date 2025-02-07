b3a157dfe6803531f3e3a1ac52342bf9aff93563
GildoSavior <gildosalva.pe@gmail.com>
2025-02-08 00:32:39 +0100

    Finish BlurredImageBackground

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/core/domain/Result.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/core/domain/Result.kt
@@ -2,7 +2,7 @@ package com.plcoding.bookpedia.core.domain
 
 sealed interface Result<out D, out E: Error> {
     data class Success<out D>(val data: D): Result<D, Nothing>
-    data class Error<out E: com.plcoding.bookpedia.core.domain.Error>(val error: E):
+    data class Error<out E: com.plcoding.bookpedia.core.domain.Error>(val error: Exception):
         Result<Nothing, E>
 }
 
