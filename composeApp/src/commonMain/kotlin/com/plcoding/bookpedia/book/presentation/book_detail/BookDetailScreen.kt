259db8ee0d71f25f4fbe38fbd57246f179168ec5
GildoSavior <gildosalva.pe@gmail.com>
2025-02-08 23:11:45 +0100

    Fix Bug on Detail Screen

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_detail/BookDetailScreen.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_detail/BookDetailScreen.kt
@@ -1,11 +1,15 @@
 
 package com.plcoding.bookpedia.book.presentation.book_detail
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.plcoding.bookpedia.book.presentation.book_detail.components.BlurredImageBackground
 import org.koin.compose.viewmodel.koinViewModel
@@ -31,7 +35,7 @@ fun BookDetailScreenRoot(
 }
 
 @Composable
-fun BookDetailScreen(
+private fun BookDetailScreen(
     state: BookDetailState,
     onAction: (BookDetailAction) -> Unit
 ) {
@@ -44,9 +48,17 @@ fun BookDetailScreen(
         onBackClick = {
             onAction(BookDetailAction.OnBackClick)
         },
-        content = {
-            // Content here
-        },
         modifier = Modifier.fillMaxWidth()
-    )
+    ) {
+        if(state.book != null) {
+            Column(
+                modifier = Modifier
+                    .widthIn(max = 700.dp)
+                    .fillMaxWidth()
+            ) {
+
+            }
+        }
+    }
+
 }
