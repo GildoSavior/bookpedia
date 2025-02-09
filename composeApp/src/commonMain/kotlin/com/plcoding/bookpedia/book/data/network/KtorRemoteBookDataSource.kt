dba94ae51b31398ea29f4b742cd98af038fbbece
GildoSavior <gildosalva.pe@gmail.com>
2025-02-09 11:57:04 +0100

    Implementing fetch book description

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/data/network/KtorRemoteBookDataSource.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/data/network/KtorRemoteBookDataSource.kt
@@ -1,5 +1,6 @@
 package com.plcoding.bookpedia.book.data.network
 
+import com.plcoding.bookpedia.book.data.dto.BookWorkDto
 import com.plcoding.bookpedia.book.data.dto.SearchResponseDto
 import com.plcoding.bookpedia.core.data.safeCall
 import com.plcoding.bookpedia.core.domain.DataError
@@ -31,6 +32,14 @@ class KtorRemoteBookDataSource(
         }
 
         return result
+    }
+
+    override suspend fun getBookDetails(bookWorkId: String): Result<BookWorkDto, DataError.Remote> {
+        return safeCall<BookWorkDto> {
+            httpClient.get(
+                 urlString = "$BASE_URL/works/$bookWorkId.json"
+            )
+        }
     }
 
 }
